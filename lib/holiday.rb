e7f4f25547d1fe308749d19ae751855f64ec0dd8
Josh Cesana <joshuacesana@gmail.com>
2018-05-21 02:45:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
         i = 0 
         while i < humanized_holiday.length 
           humanized_holiday[i].capitalize
+          binding.pry 
           i += 1 
         end 
         humanized_holiday = humanized_holiday.join(' ')
