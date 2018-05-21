4125ea5dcc22aa9ca862ec74e7e92182c0850ee9
Josh Cesana <joshuacesana@gmail.com>
2018-05-21 03:04:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies| 
-      if supplies.includes?("BBQ")
+      if supplies.include?("BBQ")
         holidays_with_BBQ << holiday 
       end 
     end 
